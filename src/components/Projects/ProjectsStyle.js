d6ed2a25ec66d689df659e386433dc72b4d8043a
ndongo78 <ndongodahs@yahoo.fr>
2021-09-01 22:27:16 +0200

    update

--- src/components/Projects/ProjectsStyle.js
+++ src/components/Projects/ProjectsStyle.js
@@ -93,12 +93,12 @@ export const UtilityList = styled.ul`
   justify-content: space-around;
   margin: 2.5rem 0;
 `;
-
+//#6b3030
 export const ExternalLinks = styled.a`
 color:#d4c0c0;
 font-size: 1.6rem;
 padding:1rem 1.5rem;
-background: #6b3030;
+background: #506AD4;
 border-radius: 15px;
 transition: 0.5s;
 &:hover{
--- src/themes/default.js
+++ src/themes/default.js
@@ -4,10 +4,10 @@ export default {
     title: "Space Grotesk, sans-serif",
     main: "Space Grotesk, sans-serif"
   },
-  // Colors for layout
+  // Colors for layout #0F1624 #010440
   colors: {
     primary1: "hsl(204,23.8%,95.9%)",
-    background1: "#0F1624",
+    background1: "#171232",
     accent1: "hsl(34.9,98.6%,72.9%)",
     button: "hsl(205.1,100%,36.1%)",
     background2: "hsl(232.7,27.3%,23.7%)",
